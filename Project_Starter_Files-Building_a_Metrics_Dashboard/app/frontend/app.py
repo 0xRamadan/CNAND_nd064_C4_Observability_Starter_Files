9cf4bcf70f1f5317eee08efed85a0736af5e8f0a
Mahmoud Abdallah <mah64725@gmail.com>
2022-09-23 19:39:11 +0200

    restructure of the project

--- Project_Starter_Files-Building_a_Metrics_Dashboard/app/frontend/app.py
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/app/frontend/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
 
 app = Flask(__name__)
-
 metrics = GunicornInternalPrometheusMetrics(app)
 
 @app.route("/")
